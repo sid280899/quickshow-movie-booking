800a36faf77c99a077455599b5dd05567aff9b03
siddhesh <siddheshneharkar70@gmail.com>
2025-07-03 19:45:07 +0530

    integrate Stripe payment gateway and add stripeWebhook controller

--- client/src/pages/SeatLayout.jsx
+++ client/src/pages/SeatLayout.jsx
@@ -86,8 +86,7 @@ const SeatLayout = () => {
           headers: { Authorization: `Bearer ${await getToken()}` }})
 
         if(data.success){
-          toast.success(data.message)
-          navigate('/my-bookings')
+          window.location.href = data.url;
         }else{
           toast.error(data.message)
         }
